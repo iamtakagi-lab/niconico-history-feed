96d3be3f1ce6306194a6b7ee2fda50792eb0e376
takagi <iam.takagi.dev@gmail.com>
2021-07-14 23:11:04 +0900

    delete: sort.ts

--- src/sort.ts
+++
@@ -1,9 +0,0 @@
-import { HistoryItem } from "./typings/niconico";
-
-export default (items: Array<HistoryItem>) => {
-    return items.sort((a, b) => {
-        if (a.watch_date < b.watch_date) return -1;
-        if (a.watch_date > b.watch_date) return 1;
-        return 0;
-    });
-}
